e1bf320872350250c553eae6adf7747d16f41b76
Timofej Jermolaev <teemozy9@gmail.com>
2021-02-11 13:41:13 +0100

    client done

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,55 @@
+import React from "react";
+
 import "./App.css";
 import Logo from "./logo.png";
 
-let requestFunds = () => {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ title: "React POST Request Example" }),
+class App extends React.Component {
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state);
   };
-  fetch("https://faucet.cheap/request", requestOptions)
-    .then((response) => response.json())
-    .then((data) => this.setState({ postId: data.id }));
-};
+  handleSubmit = (event) => {
+    fetch("https://api.faucet.cheap/request", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+    }).then(function (response) {
+      console.log(response);
+      return response.json();
+    });
+    event.preventDefault();
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <div className="logo">
-        <img src={Logo} alt="logo" />
+  render() {
+    return (
+      <div className="App">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <p>
+          Welcome to CheapETH Faucet.<br></br>
+          <small>
+            {" "}
+            Where you can get cTH cheaper than usual... cuz its free
+          </small>
+        </p>
+        <i>Address:</i>
+        <input
+          name="address"
+          type="text"
+          defaultValue="0x0"
+          onChange={this.handleChange}
+        ></input>
+        <i>cTH request amount:</i>
+        <input
+          name="amt"
+          type="number"
+          defaultValue="0"
+          onChange={this.handleChange}
+        ></input>
+        <button onClick={this.handleSubmit}>Request Funds</button>
       </div>
-      <p>
-        Welcome to CheapETH Faucet.<br></br>
-        <small> Where you can get cTH cheaper than usual... cuz its free</small>
-      </p>
-      <i>Address:</i>
-      <input id="address" type="text" defaultValue="0x0"></input>
-      <i>cTH request amount:</i>
-      <input id="amt" type="number" defaultValue="0"></input>
-      <button>Request Funds</button>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
